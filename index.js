a77869d2cd2a34027214a377d1b0040fcd89a079
HugoBzn <hugobazan1499@gmail.com>
2021-11-04 08:34:45 -0600

    S23 Sirviendo HTML

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ import http from 'http'
 //Callback (cb) es una funcion que se ejeutará ante cualquier peticion de un recusro
 //En nuestro Server (request, response)
 const server = http.createServer((req, res)=>{
+    // INforma en la consola del servidor que ha recibido unapeticion
     console.log("> Se ha recibido una peticion.");
     // Logeando el objeto req: peticion
     console.log(`Informacion de la peticion`);
     console.log(`🐱‍🐉 url: ${req.url}`);
     console.log(`🐱‍🐉 Request method: ${req.method}`);
-    console.log(`🐱‍🐉 PLataforma del cliente: ${req.headers['sec-ch-ua-platform']}`);
-    console.log(`🐱‍🐉 PLataforma del cliente: ${req.headers['sec-ch-ua-mobile']}`);
-    //Respondemos
-    res.write("Esta es la respuesta del servidor.");
+
+    //Establecer el tipo de contenido para el cliente 
+    res.setHeader('Content-Type', 'text/html');
+
+    // Envio el contenido
+    res.write("<html>");
+    res.write("<head><title>My App</title></head>");
+    //res.write("<body><h1>Hello from the server &#128519;</h1></body>");
+    res.write(`<body><h1>Hello from the server &#128519;</h1><p style="color:red">Rescurso solicitado: ${req.url}</p></body>`);
+    res.write("</html>");
+
     //Terminar conexion
     res.end();
 });
@@ -23,6 +31,6 @@ const server = http.createServer((req, res)=>{
 // Le paso un callback que se escribira en la consola
 // cuando el servidor este ecuchando
 //192.168.0.15:3000
-server.listen(3000, '0.0.0.0', () => {
-    console.log("🕵️‍♀️ Servidor escuchando en http://localhost:3000");
+server.listen(3000, '192.168.0.15', () => {
+    console.log("🕵️‍♀️ Servidor escuchando en http://192.168.0.15:3000");
 });
