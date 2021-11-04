61dcfe919bea57196f6953b2935926a39178dab5
HugoBzn <hugobazan1499@gmail.com>
2021-11-04 08:04:19 -0600

    S23 Logeando el objeto request

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ import http from 'http'
 //En nuestro Server (request, response)
 const server = http.createServer((req, res)=>{
     console.log("> Se ha recibido una peticion.");
+    // Logeando el objeto req: peticion
+    console.log(`Informacion de la peticion`);
+    console.log(`🐱‍🐉 url: ${req.url}`);
+    console.log(`🐱‍🐉 Request method: ${req.method}`);
+    console.log(`🐱‍🐉 PLataforma del cliente: ${req.headers['sec-ch-ua-platform']}`);
+    console.log(`🐱‍🐉 PLataforma del cliente: ${req.headers['sec-ch-ua-mobile']}`);
     //Respondemos
-    res.write('Hola');
+    res.write("Esta es la respuesta del servidor.");
     //Terminar conexion
     res.end();
 });
@@ -16,7 +22,7 @@ const server = http.createServer((req, res)=>{
 //3. Pongo a trabajar al servidor
 // Le paso un callback que se escribira en la consola
 // cuando el servidor este ecuchando
-//192.168.0.15
-server.listen(3000, '192.168.0.15', () => {
-    console.log("🕵️‍♀️ Servidor escuchando en http://192.168.0.15:3000");
+//192.168.0.15:3000
+server.listen(3000, '0.0.0.0', () => {
+    console.log("🕵️‍♀️ Servidor escuchando en http://localhost:3000");
 });
