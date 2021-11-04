19d62301607fdc72fdca96836238c6916834eca7
HugoBzn <hugobazan1499@gmail.com>
2021-11-04 07:19:55 -0600

    Sesion 22 Creando un servidor con http

---
+++ index.js
@@ -0,0 +1,22 @@
+//1. Importa el modulo http
+import { SSL_OP_CISCO_ANYCONNECT } from 'constants';
+import http from 'http'
+
+//2. Crea el servidor
+//Callback (cb) es una funcion que se ejeutará ante cualquier peticion de un recusro
+//En nuestro Server (request, response)
+const server = http.createServer((req, res)=>{
+    console.log("> Se ha recibido una peticion.");
+    //Respondemos
+    res.write('Hola');
+    //Terminar conexion
+    //res.end();
+});
+
+//3. Pongo a trabajar al servidor
+// Le paso un callback que se escribira en la consola
+// cuando el servidor este ecuchando
+//192.168.0.15
+server.listen(3000, '192.168.0.15', () => {
+    console.log("🕵️‍♀️ Servidor escuchando en http://192.168.0.15:3000");
+});
