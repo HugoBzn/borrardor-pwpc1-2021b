6c439a0ba287f8cc202e6b84628879675498b5df
HugoBzn <hugobazan1499@gmail.com>
2021-11-16 08:27:21 -0600

    S27 Modularizacion con export

--- index.js
+++ index.js
@@ -2,114 +2,13 @@
 import { SSL_OP_CISCO_ANYCONNECT } from 'constants';
 import http from 'http'
 import fs from "fs";
+// 2. Importando el module routes
+import routes from "./routes.js"
 
 //2. Crea el servidor
 //Callback (cb) es una funcion que se ejeutará ante cualquier peticion de un recusro
 //En nuestro Server (request, response)
-const server = http.createServer((req, res)=>{
-    // Obteniendo el recurso solicitado
-    let { url, method } = req;
-
-    // Informa en la consola del servidor que ha recibido unapeticion
-    console.log(`📮 Se ha solicitado el siguiente recurso: ${method} : ${req}`);
-
-    // FIltrar la URL
-    if (url === '/') {
-        // Respuesta ante "Get /"
-        // 1. Estableciendo el tipo de retorno como html
-        res.setHeader('Content-Type','text/html');
-        // 2. Escribiendo la respuesta 
-        res.write(`
-        <html>
-            <head>
-                <title>Enter Message</title>
-            </head>
-            <body>
-            <h1>Send Message</h1>
-            <form action="/message" method="POST">
-                <input type="text" name="message">
-                <button type="submit">send</button>
-            </form>
-            </body>
-        </html>
-        `);
-        // Cerrando la conexion
-        res.end();
-
-    }else if(url === '/message' && method === 'POST') {
-        //1. Se crea una variable para guardar los datos de entrada
-        let body = [];
-        //2. Registrar un manejador para la entrada de los datos
-        req.on("data", (chunk)=>{
-            //2.1 Registrando los trozos de datos que llega al backend
-            console.log(chunk);
-            //2.2 Acumula los datos de entrada
-            body.push(chunk);
-            //2.3 Proteccion en caso de recepcion masiva de datos
-            if (body > 1e6) req.socket.destroy();
-        });
-        // Ejecuta Operacion(ARGS1,ARGS2,ARG3,cb)
-        // Suma2Numeros(1,2,(res) => {console.log(res)})
-        /*
-        1. let res = Suma2Numeros(1,2);
-        2. Console.log(res); //undefined
-        */
-        //3. Registrando un manejador de fin de recepcion de datos
-        req.on('end', ()=>{
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            // Guardando el mensaje en un archivo
-            fs.writeFile('message.txt', message, (err)=>{
-                //Verificar que hubo eror
-                if (err) {
-                    console.log("> No se pudo grabar el archivo");
-                    res.statusCode = 500; // Internal server error
-                    res.setHeader("ERROR WHEN LOADING FILE");
-                    return res.end();
-                }
-                // En caso de no haber error
-                fs.writeFileSync('message.txt', message);
-                // Establecer el status code de redireccionamiento
-                res.statusCode = 302;
-                // Establecemos ruta de redireccionamiento
-                res.setHeader('Location', '/');
-                //Finalizo la conexion
-                return res.end();
-            });
-        });
-    } else if(url === '/author'){
-                // 1. Estableciendo el tipo de retorno como html
-                res.setHeader('Content-Type','text/html');
-                let urlImage = 'https://pbs.twimg.com/profile_images/1073208648355209217/NC_kTVjb_400x400.jpg';
-                // 2. Escribiendo la respuesta 
-                res.write('<html>');
-                res.write('<head><title>My app</title></head>');
-                res.write('<body>');
-                res.write('<h1>Author</h1>');
-                res.write('<p>Ivan Rivalcoba Rivas - Web Developer</p>');
-                res.write(`<img width="300px" src="${urlImage}" alt="Foto Ian Rivalcoba">`);
-                res.write('</body>');
-                res.write('</html>');
-        
-                // Cerrando la conexion
-                res.end();
-    }else{
-        // Recurso no encontrado
-        console.log(`❌ Recurso solicitado no encontrado ❌: ${url}`);
-
-        // Respuesta ante "Get /"
-        // 1. Estableciendo el tipo de retorno como html
-        res.setHeader('Content-Type','text/html');
-        // 2. Escribiendo la respuesta 
-        res.write('<html>');
-        res.write('<head><title>My app</title></head>');
-        res.write('<body><h1>ERROR: 404 - Recurso no encontrado</h1></body>');
-        res.write('</html>');
-
-        // Cerrando conexion
-        res.end();
-    }
-});
+const server = http.createServer(routes.requestHandler);
 
 //3. Pongo a trabajar al servidor
 // Le paso un callback que se escribira en la consola
--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
-#PWPC I
+# PWPC I
 Este será el proyecto de la materia de programación web
 en pila completa I
+
+Browsers => Ram [GLOBAL]
+
+ES3 --- ES4X
+2009 => CommmonJs ==> NODE 
+require() Permite inmportar modulos
+exports Permite exportar modulos
+
+ES5
+"export"
+1. Exportaciones nombradas
+2. Exportaciones por defecto
