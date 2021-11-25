eeb7d3f8499fcadc1d76ef8155a60c9409deacea
HugoBzn <hugobazan1499@gmail.com>
2021-11-25 08:42:10 -0600

    S31 Procesando un formulario

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ import fs from "fs";
 // 2. Importando el module routes
 import routes from "./routes.js"
 // 3. Importando Express
-import Express from 'express'
+import Express, { application } from 'express'
 import { send } from 'process';
 
 // Crear una instancia de Express
 const app = Express(); // (req, res, next) => {} request handler
+
+// 1. Insertando Middleware para la lectura de datos 
+// desde un cliente
+app.use(Express.urlencoded({extended: false}));
+
+// Loggin de peticiones
+app.use((req,_,next)=>{
+    console.log(`📞 Se ha realizado la peticion: "${req.method} : ${req.path}"`);
+    next();
+});
 
 // Se debe colocar primero ya que el orden de registro
 // Determina el orden de verificacion
@@ -17,9 +27,32 @@ app.use('/about',(_,res)=>{
     res.send("<h1>💡 Acerca de...</h1>\n 🙋‍♂️ Sitio inicial hecho con NodeJs")
 });
 
+// Sirviendo recurso de formulario
+app.use('/add-student-form', (_, res)=>{
+    res.send(`
+    <form action="/add-student" method="POST">
+    <label for="student-name">👩‍🎓 Student Name</label>
+    <input type="text" name="name" id="student-name">
+    <button type="submit">Add student</button>
+    </form>
+    `);
+});
+
+// Ruta que procesa el formulario
+app.use('/add-student', (req, res) => {
+    // Iterando sobre todo el objeto
+    for(const prop in req.body){
+        console.log(`🚩 ${prop}: ${req.body[prop]}`);
+    }
+    console.log(`🚩 Metodo}: ${req.method}`);
+
+    res.json(req.body);
+    // Realizamos un redireccionamiento
+    // res.redirect('/');
+});
+
 // La ruta raiz entra en todo tipo de peticion
 app.use(['/','/home'],(_,res)=>{
-    console.log('📞 Emitiendo respuesta a cliente: "/"');
     res.send("<h1>Mi APP</h1>\n 🙋‍♂️ Bienvenido a este sitio");
 });
 
